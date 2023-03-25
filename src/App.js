414f61782031e8e893907851bc851ee0e54e411f
Kingsley <itksweb@gmail.com>
2023-03-25 15:44:17 +0100

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import { Fragment, useState } from "react";
+import Calc from "./components/Calc";
 
 function App() {
+  const [display, setDisplay] = useState("0");
+
+  const updateDisplay = (val) => {
+    let beforeOp = /^(0\.|[1-9][0-9]*\.?)(\d*)\.?(\d*)/g;
+    let afterOp = /([\+\-\*\/])(0\.|[1-9][0-9]*\.?)?(\d*)(?:\.?(\d*))$/g;
+    let op = /[\-\+\*\/]+([\+\*\/])/;
+    if (display === "0") {
+      setDisplay(val.target.value);
+    } else {
+      setDisplay((prev) => prev + val.target.value);
+    }
+    if (beforeOp.test(display)) {
+      setDisplay((prev) => prev.replace(beforeOp, "$1$2$3"));
+    }
+    if (afterOp.test(display)) {
+      setDisplay((prev) => prev.replace(afterOp, "$1$2$3$4"));
+    }
+    if (op.test(display)) {
+      setDisplay((prev) => prev.replace(op, "$1"));
+    }
+  };
+  const resetDisplay = () => {
+    return setDisplay("0");
+  };
+  const deleteDisplay = () => {
+    if (display.length > 1) {
+      setDisplay((prev) => prev.replace(/.$/, ""));
+    } else if (display.length === 1) {
+      return setDisplay("0");
+    }
+  };
+  const solveIt = () => {
+    setDisplay((prev) => eval(prev));
+  };
+  const manageDot = (val) => {
+    if (display === "0") {
+      return setDisplay("0.");
+    } else if (display[display.length - 1] === ".") {
+      return;
+    } else {
+      setDisplay((prev) => (prev += val.target.value));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Calc
+        display={display}
+        updateDisplay={updateDisplay}
+        resetDisplay={resetDisplay}
+        deleteDisplay={deleteDisplay}
+        solveIt={solveIt}
+        manageDot={manageDot}
+      />
+    </Fragment>
   );
 }
 
---
+++ src/StyleLogic.js
@@ -0,0 +1,33 @@
+let wrapper = {
+  backgroundColor: "hsl(222, 26%, 31%)",
+  color: "white",
+};
+let themeSwitcher = {
+  backgroundColor: "hsl(223, 31%, 20%)",
+  justifyContent: "flex-start",
+};
+let themeSwitcherInner = {
+  backgroundColor: "hsl(6, 63%, 50%)",
+};
+let calcDisplay = {
+  color: "white",
+  backgroundColor: "hsl(224, 36%, 15%)",
+};
+let keypad = {
+  backgroundColor: "hsl(223, 31%, 20%)",
+};
+let inputBtn = {
+  backgroundColor: "#eae3da",
+  color: "#464852",
+  boxShadow: "0 4px #b1a399",
+};
+let colvar = {
+  color: "white",
+  backgroundColor: "hsl(225, 21%, 49%)",
+  boxShadow: "0 4px hsl(224, 28%, 35%)",
+};
+let equal = {
+  color: "white",
+  backgroundColor: "hsl(6, 63%, 50%)",
+  boxShadow: "0 4px hsl(6, 70%, 34%)",
+};
